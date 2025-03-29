dc9259c54efa00c9290ddbe10ad6b4c0ab9304ae
Ela <eunseolee90917@gmail.com>
2025-03-29 09:30:11 +0900

    Initial Commit

---
+++ SettingView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SettingView
+//
+//  Created by Ella's Mac on 3/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SettingView/SettingViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SettingViewApp.swift
+//  SettingView
+//
+//  Created by Ella's Mac on 3/29/25.
+//
+
+import SwiftUI
+
+@main
+struct SettingViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
